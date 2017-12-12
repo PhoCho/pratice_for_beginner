52e1107da5690f494c240298ac8e50d93a7fdf45
ye min khaung (aka) pho cho <ye.min.khaungba@gmail.com>
2017-12-12 23:15:31 +0630

    explore n,c,print,without print in python

---
+++ import_pdb_set_trace.py
@@ -0,0 +1,23 @@
+# run this script with (python script_name)
+#type is not code, just command reader
+import pdb
+pdb.set_trace()
+
+print("n for next")
+
+number = 1+1
+print("type print number")
+
+print("type number")
+
+
+print("type c")
+
+print("this is c")
+print("this is c")
+print("this is c")
+
+print("do you know what is diff between c and n?")
+
+
+
